ce77ba32273b7abfb1ab59d2d236752e5b860688
Tu Nombre <ja007735@gmail.com>
2024-12-05 13:55:59 -0600

    2

--- mi-primer-api-rest/src/main/java/com/jad/service/impl/ClienteImpl.java
+++ mi-primer-api-rest/src/main/java/com/jad/service/impl/ClienteImpl.java
@@ -5,22 +5,27 @@ import com.jad.model.entity.Cliente;
 import com.jad.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClienteImpl implements ICliente {
 
     @Autowired
     private ClienteDao clienteDao;
+
+    @Transactional
     @Override
     public Cliente save(Cliente cliente) {
         return clienteDao.save(cliente);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Cliente findById(Integer id) {
         return clienteDao.findById(id).orElse(null);
     }
 
+    @Transactional
     @Override
     public void delete(Cliente cliente) {
         clienteDao.delete(cliente);
